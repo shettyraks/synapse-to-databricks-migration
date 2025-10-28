15727ea6d8122c409a197a0f766ab9a22175b543
shettyraks <shetty.rakshu@gmail.com>
2025-10-28 16:29:46 +0700

    migrations: remove main catalog qualifiers; add flyway repair; adjust schemas/defaultSchema; remove redundant V21 header table script

--- scripts/deploy-sql-migrations.sh
+++ scripts/deploy-sql-migrations.sh
@@ -117,7 +117,7 @@ flyway.url=jdbc:databricks://${DATABRICKS_HOST}:443;transportMode=http;ssl=1;htt
 flyway.driver=com.databricks.client.jdbc.Driver
 flyway.locations=${FLYWAY_LOCATIONS}
 flyway.schemas=${SCHEMAS}
-flyway.defaultSchema=flyway_${CATALOG}
+flyway.defaultSchema=flyway_${CUSTOMER}
 flyway.baselineOnMigrate=true
 flyway.validateOnMigrate=true
 flyway.outOfOrder=false
