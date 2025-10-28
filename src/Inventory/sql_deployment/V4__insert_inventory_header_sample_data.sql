176c8474b8a9cafda9f7adf77e28451505a5b927
shettyraks <shetty.rakshu@gmail.com>
2025-10-28 17:29:55 +0700

    Inventory: update sample data in V4__insert_inventory_header_sample_data.sql

--- src/Inventory/sql_deployment/V4__insert_inventory_header_sample_data.sql
+++ src/Inventory/sql_deployment/V4__insert_inventory_header_sample_data.sql
@@ -1,5 +1,5 @@
 -- Create Inventory Header Table
-CREATE TABLE IF NOT EXISTS inventory.inventory_header1 (
+CREATE TABLE IF NOT EXISTS inventory.inventory_header (
     inventory_id STRING NOT NULL,
     location_id STRING NOT NULL,
     fiscal_period STRING NOT NULL,
